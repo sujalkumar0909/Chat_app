91fb1ce55ae3e3fd505e59a6eef07588852724f4
sujalkumar0909 <sujalkumar7209@gmail.com>
2024-08-21 12:03:23 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDp7-LxN3DxNlGLEfg0eYqgZO0rDaK5vow",
-  authDomain: "reactchatapp-53928.firebaseapp.com",
-  projectId: "reactchatapp-53928",
-  storageBucket: "reactchatapp-53928.appspot.com",
-  messagingSenderId: "249886441603",
-  appId: "1:249886441603:web:0535838b46dbfeda265bb6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
