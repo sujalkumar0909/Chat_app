13c29bf4ef3bfe887f0e604a3557a8c803c0d918
sujalkumar0909 <sujalkumar7209@gmail.com>
2024-08-21 12:11:57 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,1 +1,21 @@
 //create your firebase connection
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth=getAuth()
+export const db=getFirestore()
+export const storage=getStorage()
